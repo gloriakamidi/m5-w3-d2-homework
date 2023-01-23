382cbd9d4a1e2dbff0ec1d9cc75e800f97d91bf1
gloriakamidi <g.kamidi.312@westcliff.edu>
2023-01-22 20:48:56 -0800

    w3d2hw

---
+++ crud-json-server/src/App.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Lists from "./Lists";
+import CreateList from "./CreateList";
+
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        loading: false,
+        alldata: [],
+        singledata: {
+          title: "",
+          author: ""
+        }
+      };
+    }
+   // Handler
+    getLists = () => {
+      fetch("http://localhost:5000/posts")
+      .then(res => res.json())
+      .then(result => 
+        this.setState({
+          loading: false,
+          alldata: result
+        })
+      )
+      .catch(console.log);
+    }
+    render() {
+      const listTable = this.state.loading ? (
+        <span>Loading Data......Please be patient.</span>
+      ) : (
+        <Lists alldata = {this.state.alldata} />
+      );
+      
+      return (
+        <div className="container">
+          <span className="title-bar">
+            <button 
+            type="button" 
+            className="btn btn-primary"
+            onClick={this.getLists}>
+              Get Lists
+            </button>
+            <CreateList singledata={this.state.singledata} />
+          </span>
+          {listTable}
+        </div>
+      );
+    }
+  }
+
+export default App;
